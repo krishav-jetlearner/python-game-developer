c2e1f2d0fbaf9bef345d7e9ec173528b01f30c8b
krishav-jetlearner <krishav.lihala@gmail.com>
2026-08-20 17:54:52 +0200

    Add files via upload

---
+++ classtwo.py
@@ -0,0 +1,10 @@
+geography = {}
+while True:
+    print("1.Insert a value")
+    print("2.Retrieve a value")
+    print("3.delete a value")
+    print("4.view all values")
+    print("5.exit")
+    num = int(input("Choose 1-5"))
+    if num == 1:
+        
