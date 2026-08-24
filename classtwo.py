514b28b0850237e5a3271c2a590f92fa79b990e8
krishav-jetlearner <krishav.lihala@gmail.com>
2026-08-24 18:40:47 +0200

    Add files via upload

--- classtwo.py
+++ classtwo.py
@@ -5,6 +5,18 @@ while True:
     print("3.delete a value")
     print("4.view all values")
     print("5.exit")
-    num = int(input("Choose 1-5"))
+    num = int(input("Choose 1-5 "))
     if num == 1:
-        
+        country = input("write any country ")
+        capital = input("write the capital of the country ")
+        geography[country] = capital
+    elif num == 2:
+        retrieve = input("write the country you want to see its capital ")
+        print(geography[retrieve])
+    elif num == 3:
+        delete = input("write the country that you want deleted ")
+        del geography[delete]
+    elif num == 4:
+        print(geography)
+    elif num == 5:
+        break
